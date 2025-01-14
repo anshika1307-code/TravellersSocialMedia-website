071d13242aec51318dfe89109a4d161c51b727e2
Vaibhav Mali <malivaibhav.7620@gmail.com>
2025-01-15 00:17:53 +0530

    frontend changes

--- src/components/Timeline/PostCard.jsx
+++ src/components/Timeline/PostCard.jsx
@@ -1,26 +1,31 @@
-
-
-import React, { useState } from 'react';
-import DairySpiralImage from '../../images/spiral.png';
-import DairySpiralImage2 from '../../images/spiral-hori.png';
-import Postimage from '../../images/1.jpg';
-import Postvideo from '../../images/2.mp4';
-import Postimage2 from '../../images/3.jpg';
-import Postimage3 from '../../images/4.jpg';
-import topL1 from '../../images/topL1.png';
-import experience from '../../assets/experience.png';
-import cost from '../../assets/cost.png';
-import location from '../../assets/location.png';
-import message from '../../assets/message.png';
-import more from '../../assets/more.png';
-import like from '../../assets/like.png';
-import comment from '../../assets/comment.png';
-import share from '../../assets/share.png';
-import avatar from '../../assets/avatar.webp';
-import { Carousel } from 'react-responsive-carousel';
+import React, { useState } from "react";
+import DairySpiralImage from "../../images/spiral.png";
+import DairySpiralImage2 from "../../images/spiral-hori.png";
+import Postimage from "../../images/1.jpg";
+import Postvideo from "../../images/2.mp4";
+import Postimage2 from "../../images/3.jpg";
+import Postimage3 from "../../images/4.jpg";
+import topL1 from "../../images/topL1.png";
+import experience from "../../assets/experience.png";
+import cost from "../../assets/cost.png";
+import location from "../../assets/location.png";
+import message from "../../assets/message.png";
+import more from "../../assets/more.png";
+import like from "../../assets/like.png";
+import comment from "../../assets/comment.png";
+import share from "../../assets/share.png";
+import avatar from "../../assets/avatar.webp";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { BsFillPersonVcardFill } from "react-icons/bs";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { FaCircleDollarToSlot } from "react-icons/fa6";
+import { MdFoodBank } from "react-icons/md";
+import { AiFillLike } from "react-icons/ai";
+import { IoPersonCircleSharp } from "react-icons/io5";
+
 const PostCard = ({ post }) => {
-  const [selectedButton, setSelectedButton] = useState('Post');
+  const [selectedButton, setSelectedButton] = useState("Post");
 
   const handleButtonClick = (buttonName) => {
     setSelectedButton(buttonName);
@@ -58,7 +63,7 @@ const PostCard = ({ post }) => {
     { type: "image", src: topL1 },
   ];
   console.log("post data", post);
-  return  (
+  return (
     <div className="flex-1 h-full mt-4 mb-4 flex flex-row">
       {/* Sidebar */}
       <div className="lg:hidden w-1/6 p-4 border-r border-gray-300">
@@ -90,107 +95,145 @@ const PostCard = ({ post }) => {
         </div>
       </div>
       <div className="flex flex-col lg:flex-row h-full mb-8 ml-3 relative w-5/6 lg:w-full">
-      <div className="lg:flex lg:items-stretch w-full">
-        {/* Picture Preview Section */}
-        
-          <div className="w-full lg:w-1/2 border border-gray-300 rounded-lg shadow-lg overflow-hidden relative">
-  {post?.media?.length > 0 ? (
-    <Carousel showThumbs={false} autoPlay infiniteLoop showStatus={false}>
-      {post.media.map((url, index) => (
-        <div key={index} className="relative w-full" style={{ paddingTop: '100%' }}>
-          {url.endsWith(".mp4") || url.endsWith(".webm") || url.endsWith(".ogg") ? ( // Check if the URL is a video
-            <video className="absolute top-0 left-0 w-full h-full object-cover" controls>
-              <source src={url} type="video/mp4" />
-            </video>
-          ) : (
-            <img src={url} alt={`Slide ${index}`} className="absolute top-0 left-0 w-full h-full object-cover" />
-          )}
-        </div>
-      ))}
-    </Carousel>
-  ) : (
-    <p className="text-center py-4">No media available</p>
-  )}
-</div>
+        <div className="lg:flex lg:items-stretch w-full">
+          {/* Picture Preview Section */}
 
+          <div className="w-full lg:w-1/2 border border-gray-300 rounded-lg shadow-lg overflow-hidden relative">
+            {post?.media?.length > 0 ? (
+              <Carousel
+                showThumbs={false}
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+              >
+                {post.media.map((url, index) => (
+                  <div
+                    key={index}
+                    className="relative w-full"
+                    style={{ paddingTop: "100%" }}
+                  >
+                    {url.endsWith(".mp4") ||
+                    url.endsWith(".webm") ||
+                    url.endsWith(".ogg") ? ( // Check if the URL is a video
+                      <video
+                        className="absolute top-0 left-0 w-full h-full object-cover"
+                        controls
+                      >
+                        <source src={url} type="video/mp4" />
+                      </video>
+                    ) : (
+                      <img
+                        src={url}
+                        alt={`Slide ${index}`}
+                        className="absolute top-0 left-0 w-full h-full object-cover"
+                      />
+                    )}
+                  </div>
+                ))}
+              </Carousel>
+            ) : (
+              <p className="text-center py-4">No media available</p>
+            )}
+          </div>
 
           {/* Spiral */}
           {/* <img src={DairySpiralImage} alt="Dairy Spiral" className="absolute lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2 lg:w-16 lg:h-full top-0 left-1/2 transform -translate-x-1/2 w-full h-16 rotate-90 lg:rotate-0" /> */}
           <img
             src={DairySpiralImage}
             alt="Dairy Spiral"
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-16 h-full hidden lg:block"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-full hidden lg:block"
           />
           {/* <img src={DairySpiralImage2} alt="Dairy Spiral" className="absolute top-96 left-1/2 transform -translate-x-1/2 w-full h-16 lg:hidden" /> */}
           {/* Post Details Section */}
-          <div className="w-full lg:w-1/2  h-full">
+          <div className="w-full lg:w-[47%]  h-full">
             {/* Post Caption */}
 
             <div className="p-4 -mb-3">
-              <h2 className="text-[20px] font-semibold pl-6">{post.mainLocation}</h2>
-              <p className="text-gray-700 text-sm lg:text-[12px] pl-6">
+              <div className="mb-2 ml-4 flex justify-start items-center align-items-center">
+                <IoPersonCircleSharp size={40}/>
+               <div className="ml-1 -mt-2"> <p className="font-semibold text-[20px]">{post.user.username}</p>
+               <p className="text-[8px] -mt-1">2 hours ago</p></div>
+                </div>
+                
+              <h2 className="text-[20px] font-semibold pl-6">
+                {post.mainLocation}
+              </h2>
+              <p className="text-gray-700 text-sm lg:text-[14px] pl-6">
                 {post.caption}
               </p>
             </div>
             <div className="flex p-2 space-x-2 lg:pl-9 mb-2">
               <button
-                className={`rounded-lg bg-gray-200 border-2 border-gray-400 text-gray-600 text-[12px] items-center  px-2 py-1 flex ${
+                className={`rounded-lg bg-pink-200 border-2 border-gray-300 text-gray-600 text-[12px] items-center h-10 px-2  flex ${
                   selectedButton === "Experience"
                     ? "bg-white border border-gray-400"
                     : ""
                 }`}
                 onClick={() => handleButtonClick("Experience")}
               >
-                <img src={experience} className="m-1 w-5" alt="" />
+                <div className="m-1">
+                <BsFillPersonVcardFill size={18}/>
+                </div>
+
                 Experience
               </button>
+
               <button
-                className={`rounded-lg bg-gray-200 border-2 border-gray-400 text-gray-600 px-2 py-1 text-[12px] items-center flex ${
-                  selectedButton === "Cost"
-                    ? "bg-white border border-gray-400"
-                    : ""
-                }`}
-                onClick={() => handleButtonClick("Cost")}
-              >
-                <img src={cost} className="m-1 w-5" alt="" />
-                Cost
-              </button>
-              <button
-                className={`rounded-lg bg-gray-200 border-2 border-gray-400 text-gray-600 px-2 py-1 text-[12px] items-center flex ${
+                className={`rounded-lg bg-blue-200 border-2 border-gray-300 text-gray-600 px-2 py-1 text-[12px] h-10 items-center flex ${
                   selectedButton === "Places"
                     ? "bg-white border border-gray-400"
                     : ""
                 }`}
                 onClick={() => handleButtonClick("Places")}
               >
-                <img src={location} className="m-1 w-5" alt="" />
+                <div className="m-1">
+                <FaMapLocationDot size={18}/>
+                </div>
                 Places
               </button>
               <button
-                className={`rounded-lg bg-gray-200 border-2 border-gray-400 text-gray-600 px-2 py-1 text-[12px] items-center flex ${
+                className={`rounded-lg bg-green-200 border-2 border-gray-300 text-gray-600 px-2 py-1 text-[12px] h-10 items-center flex ${
+                  selectedButton === "Cost"
+                    ? "bg-white border border-gray-400"
+                    : ""
+                }`}
+                onClick={() => handleButtonClick("Cost")}
+              >
+                <div className="m-1">
+                <FaCircleDollarToSlot size={18}/>
+                </div>
+                Expenses
+              </button>
+              <button
+                className={`rounded-lg bg-orange-200 border-2 border-gray-300 text-gray-600 px-2 py-1 text-[12px] h-10 items-center flex ${
                   selectedButton === "Messages"
                     ? "bg-white border border-gray-400"
                     : ""
                 }`}
                 onClick={() => handleButtonClick("Messages")}
               >
-                <img src={message} className="m-1 w-5" alt="" />
-                Messages
+                <div className="m-1">
+                <MdFoodBank size={20}/>
+                </div>
+                Food/Stay
               </button>
               <button
-                className={`rounded-lg bg-gray-200 border-2 border-gray-400 text-gray-600 px-2 py-1 flex ${
+                className={`rounded-lg bg-red-200 border-2 border-gray-300 text-gray-600 px-2 py-1 flex text-[12px] h-10 items-center ${
                   selectedButton === "More"
                     ? "bg-white border border-gray-400"
                     : ""
                 }`}
                 onClick={() => handleButtonClick("More")}
               >
-                <img src={more} className="m-1 w-5" alt="" />
+                <div className="m-1">
+                <AiFillLike size={20}/>
+                </div>
+                Recommendation
               </button>
             </div>
-            <div className="rounded-xl border-2 border-gray-300 mx-2 ml-9 shadow-md">
+            <div className="mx-2 ml-9">
               {/* Other Post Details (Experience, Cost, Place, Message etc.) */}
-              <div className="flex items-center text-gray-600 p-4 text-[14px]">
+              <div className="flex items-center text-gray-600 p-4 text-[12px]">
                 <svg
                   className="w-4 h-4 mr-2"
                   fill="none"
@@ -213,31 +256,8 @@ const PostCard = ({ post }) => {
               </div>
             </div>
             <div className="mx-2 ml-9 rounded-xl border-2 border-gray-300 shadow-md">
-              <div className="flex flex-col items-center p-4">
-                <div className="w-full h-2 bg-gray-200 rounded-lg relative">
-                  <div
-                    className="absolute bg-rating-gradient h-2 rounded-lg"
-                    style={{ width: `${percentage}%` }}
-                  ></div>
-                  {/* <div
-          className="absolute -top-1 -translate-x-1/2 bg-rating-gradient w-4 h-4 rounded-full"
-          style={{ left: `${percentage}%` }}
-        ></div> */}
-                  <div
-                    className="absolute -top-3 text-xl"
-                    style={{
-                      left: `${percentage}%`,
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    {getEmoji(rating)}
-                  </div>
-                </div>
-                <div className="mt-2 text-lg font-semibold">
-                  Rating: {rating}
-                </div>
-              </div>
-              <div className="px-6 py-4 text-[14px]">
+           
+              <div className="px-4 py-2 text-[14px]">
                 <p>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Cumque odio molestiae consequatur labore error. Ab quaerat
@@ -252,26 +272,26 @@ const PostCard = ({ post }) => {
                 </p>
               </div>
             </div>
-            <div className="rounded-b-lg rounded-xl border-2 border-gray-300 shadow-md mx-2 border-collapse ml-9 text-sm">
+            <div className="mx-2 ml-9 text-sm">
               {/* Like, Comment, Share Icons */}
-              <div className="flex px-2 py-2 justify-center items-center">
+              <div className="flex px-2 py-2 justify-start items-center mt-2">
                 <button className="flex items-center justify-center align-items-center mr-4 text-gray-600">
                   <img src={like} className="m-1 w-6 " alt="" />
-                  <span>Like</span>
+                  <span className=" text-[11px] -mt-1 ml-1">Like</span>
                 </button>
                 <button className="flex items-center mr-4 text-gray-600">
                   {/* <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h16M4 16h16"></path>
                 </svg> */}
                   <img src={comment} className="m-1 w-6" alt="" />
-                  <span>Comment</span>
+                  <span className=" text-[11px] -mt-1 ml-1">Comment</span>
                 </button>
                 <button className="flex items-center text-gray-600">
                   {/* <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                 </svg> */}
                   <img src={share} className="m-1 w-6" alt="" />
-                  <span>Share</span>
+                  <span className=" text-[11px] -mt-1 ml-1">Share</span>
                 </button>
               </div>
             </div>
@@ -283,24 +303,3 @@ const PostCard = ({ post }) => {
 };
 
 export default PostCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
